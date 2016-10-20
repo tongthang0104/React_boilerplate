c29fa4876a46d8f50ea02bfd78d1f2e1c24962ab
Thang Tong <tongthang0104@gmail.com>
2016-10-19 21:18:58 -0700

    (feat) Complete setup

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import styles from './app.css';
+import React, {Component} from 'react';
+
+export default class App extends Component {
+  render() {
+    return <h1 className={styles.app}>Hello world!</h1>;
+  }
+};
